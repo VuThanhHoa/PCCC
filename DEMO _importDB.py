d12d2d104b35575dd865c4907cf4cf720f2674d1
Hoa <thanhhoavu112@gmail.com>
2023-12-07 10:41:21 +0700

    Update 07.12

--- DEMO _importDB.py
+++ DEMO _importDB.py
@@ -65,9 +65,9 @@ for idx in range(len(staff_df)):
 
 # Insert accounts
 accounts = [
-    {"MaNV": "P0872", "HoTen": "Hồ Anh Phát", "BoPhan": "THOP", "PhongBan": "Phòng Tổng hợp", "PassWord": "123", "Role": "admin"},
-    {"MaNV": "H0844", "HoTen": "Nguyễn Thị Diệu Hương", "BoPhan": "XN", "PhongBan": "Xí nghiệp", "PassWord": "123", "Role": "user"},
-    {"MaNV": "L1349", "HoTen": "Nguyễn Thị Na Ly", "BoPhan": "XN1", "PhongBan": "Xí nghiệp", "PassWord": "123", "Role": "user"},
+    {"MaNV": "H3803", "HoTen": "Vũ Thanh Hoà", "BoPhan": "THOP", "PhongBan": "Phòng Tổng hợp", "PassWord": "123", "Role": "admin"},
+    {"MaNV": "O0135", "HoTen": "Phan Thị Kim Oanh", "BoPhan": "XN", "PhongBan": "Xí nghiệp", "PassWord": "123", "Role": "user"},
+    {"MaNV": "L1382", "HoTen": "Nguyễn Thị Lanh", "BoPhan": "XN1", "PhongBan": "Xí nghiệp", "PassWord": "123", "Role": "user"},
 ]
 
 for acc in accounts:
